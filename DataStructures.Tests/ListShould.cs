0abcf45d3071f88ae869b6925e67a9dfe229d6a8
Matthew Butt <matthew@matthewbutt.com>
2018-09-28 16:14:54 +0100

    Implement `Map`

--- DataStructures.Tests/ListShould.cs
+++ DataStructures.Tests/ListShould.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using FluentAssertions;
 using FsCheck;
 using FsCheck.Xunit;
@@ -22,5 +22,21 @@ namespace DataStructures.Tests
         [Property]
         public void ListRef2(object item1, object item2, object item3) =>
             List.Of(item1, item2, item3).ListRef(2).Should().Be(item3);
+
+        [Property]
+        public void HaveLength(object[] items) =>
+            List.Of(items).Length().Should().Be(items.Length);
+
+        [Property]
+        public void Append(object[] items1, object[] items2) => 
+            List.Of(items1).Append(List.Of(items2)).Length().Should().Be(items1.Length + items2.Length);
+
+        [Property]
+        public void MapLength(int[] items, Func<int, int> proc) => 
+            List.Of(items).Map(proc).Length().Should().Be(items.Length);
+
+        [Property]
+        public void MapCar(NonEmptyArray<int> items, Func<int, int> proc) =>
+            List.Of(items.Get).Map(proc).Car.Should().Be(proc(items.Get[0]));
     }
 }
--- DataStructures/List.cs
+++ DataStructures/List.cs
@@ -1,17 +1,39 @@
+using System;
+
 namespace DataStructures
 {
     public static class List
     {
-        public static List<T> Of<T>(params T[] items) => ConsRec(null, items.Length - 1, items);
+        public static List<T> Of<T>(params T[] items) => ConsIter(new List<T>(null), items.Length - 1, items);
 
-        private static List<T> ConsRec<T>(List<T> currentCons, int index, T[] items)
-        {
-            if (index < 0) return currentCons;
+        private static List<T> ConsIter<T>(List<T> currentCons, int index, T[] items) =>
+            index < 0
+                ? currentCons
+                : ConsIter(new List<T>(Cons.Of(items[index], currentCons)), index - 1, items);
 
-            return ConsRec(new List<T>(Cons.Of(items[index], currentCons)), index - 1, items);
-        }
+        public static List<T> Append<T>(this List<T> list1, List<T> list2) =>
+            list1.Null
+                ? list2
+                : new List<T>(Cons.Of(list1.Car, list1.Cdr.Append(list2)));
+
+        public static T ListRef<T>(this List<T> list, int index) =>
+            index == 0
+                ? list.Car
+                : list.Cdr.ListRef(index - 1);
+
+        public static int Length<T>(this List<T> list) => LengthIter(list, 0);
+
+        private static int LengthIter<T>(List<T> list, int runningTotal) =>
+            list.Null
+                ? runningTotal
+                : LengthIter(list.Cdr, runningTotal + 1);
+
+        public static List<TOut> Map<TOut, T>(this List<T> list, Func<T, TOut> proc) =>
+            list.Null
+                ? new List<TOut>(null)
+                : new List<TOut>(Cons.Of(proc(list.Car), list.Cdr.Map(proc)));
     }
-    
+
     public class List<T>
     {
         private readonly Cons<T, List<T>> _cons;
@@ -19,14 +41,9 @@ namespace DataStructures
         internal List(Cons<T, List<T>> cons) => _cons = cons;
 
         public T Car => _cons.Car;
+        
         public List<T> Cdr => _cons.Cdr;
-
-        public T ListRef(int index)
-        {
-            if(index == 0)
-                return Car;
-
-            return Cdr.ListRef(index - 1);
-        }
+        
+        public bool Null => _cons is null;
     }
 }
